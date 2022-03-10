3bddee4f8de565d8f5cf0b11288bfab25989f23f
unknown <dlord4242@gmail.com>
2022-03-10 11:50:29 +0900

    Test

--- src/hooks/outside-click-hook.ts
+++ src/hooks/outside-click-hook.ts
@@ -1,26 +1,28 @@
-import { useEffect, useRef } from 'react';
+// import { useEffect, useRef } from 'react';
 
-export const useOutsideClickHandler = (
-  element: HTMLElement | null,
-  callback: () => void
-) => {
-  const callbackRef = useRef(callback);
+// export const useOutsideClickHandler = (
+//   element: HTMLElement | null,
+//   callback: () => void
+// ) => {
+//   const callbackRef = useRef(callback);
 
-  useEffect(() => {
-    callbackRef.current = callback;
-  }, [callback]);
+//   useEffect(() => {
+//     callbackRef.current = callback;
+//   }, [callback]);
 
-  useEffect(() => {
-    if (!element) return;
+//   useEffect(() => {
+//     if (!element) return;
 
-    const outsideClickHandler = (event: MouseEvent) => {
-      !element.contains(event.target as Node) && callbackRef.current();
-    };
+//     const outsideClickHandler = (event: MouseEvent) => {
+//       !element.contains(event.target as Node) && callbackRef.current();
+//     };
 
-    document.addEventListener('click', outsideClickHandler);
+//     document.addEventListener('click', outsideClickHandler);
 
-    return () => {
-      document.removeEventListener('click', outsideClickHandler);
-    };
-  }, [element]);
-};
+//     return () => {
+//       document.removeEventListener('click', outsideClickHandler);
+//     };
+//   }, [element]);
+// };
+
+export const foo = () => {};
